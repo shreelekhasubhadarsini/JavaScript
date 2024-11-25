be77cc0cc34282158c0704702a6833fddcbfb1d5
shreelekhasubhadarsini <shreelekhasubhadarsini39@gmail.com>
2024-11-25 20:29:18 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=122143;
+let accountMail="Shreelekhasubhadarsini39@gmail.com";
+var accountPassword=23456;
+accountCity="Bangalore";
+
+console.log(accountId);
+
+accountCity="Bhubaneswar"
+
+console.table([accountId,accountMail,accountPassword,accountCity])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS Code as newer version
+//alert(3+3) we are using nodejs,not browser
+console.log(3+3)
+console.log("Shreelekha")
+
+let name="shree"
+let age=24
+let isLoggedIn=false
+let state=null
+/* number = 2 to power 53
+   bigint
+   string = ""
+   boolean = false/true
+   null = standalone value
+   undefined =
+   symbol= unique
+
+   object
+ */
+
+   console.log(typeof "shreelekha");
+   console.log(typeof age);
+   console.log(typeof null);
+   console.log(typeof undefined);
+   
+   
+   
+   
